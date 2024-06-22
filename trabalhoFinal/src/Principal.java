90a05533a62ee1980e25b6fcb8f1045a4dd1ae1d
ciocosta <ea690125@gmail.com>
2024-06-21 22:13:38 -0300

    criado a classe cadastroFilme e comeco da classe pricipal

---
+++ trabalhoFinal/src/Principal.java
@@ -0,0 +1,10 @@
+public class Principal {
+
+    public static void menu(){
+        System.out.println("\nmenu de escolhas: ");
+        System.out.println("\nCadastrar: ");
+        System.out.println("\nBuscar: ");
+        System.out.println("\nListar todos: ");
+        System.out.println("\nExcluir: ");
+    }
+}
