63a0c71b7c4cc867b3d089b304ed086ed956e3d4
lucascoelhofelix <167718769+lucascoelhofelix@users.noreply.github.com>
2024-06-22 12:03:27 -0300

    Organizar Nomes

--- trabalhoFinal/src/Principal.java
+++ trabalhoFinal/src/Principal.java
@@ -1,7 +1,7 @@
 public class Principal {
 
     public static void menu(){
-        System.out.println("\nmenu de escolhas: ");
+        System.out.println("\nMenu De Escolhas: ");
         System.out.println("\nCadastrar: ");
         System.out.println("\nBuscar: ");
         System.out.println("\nListar todos: ");
